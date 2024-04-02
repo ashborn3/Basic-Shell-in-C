2b9820ef263df27fb5a76af8728fb5041ce664bf
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-04-02 21:14:52 +0530

    Added Required Header Files

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
 
 int main() {
     printf("LET US SHELL!\n");
